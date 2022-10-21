f8c58a282dd763f20ca2e00fff5f385313923479
hp <hp@192.168.1.36>
2022-10-21 11:58:51 +0530

    Added Two more test cases

---
+++ E2ESeleniumJenkins/src/testcases/TestCase5.java
@@ -0,0 +1,24 @@
+package testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestCase5 
+{
+	@Test
+	public void a()
+	{
+	WebDriverManager.chromedriver().setup();
+	WebDriver driver = new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.get("https://www.facebook.com/");
+	driver.findElement(By.id("email")).sendKeys("vishaltest@gmail.com");
+	driver.findElement(By.id("pass")).sendKeys("12434");
+	driver.quit();//1234
+	}
+
+}
